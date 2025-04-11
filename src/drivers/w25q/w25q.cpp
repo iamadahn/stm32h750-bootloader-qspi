eb811b0defa87ff9cf082afde868bd9255aa9d75
iamadahn <vendonism16@yandex.ru>
2025-04-11 22:10:07 +0300

    drivers: w25q: refactored the driver

--- src/drivers/w25q/w25q.cpp
+++ src/drivers/w25q/w25q.cpp
@@ -1,388 +1,391 @@
 #include "w25q.h"
 
-extern QSPI_HandleTypeDef hqspi;
+using namespace W25Q;
 
-/**
- * @brief	reset the w25q64 controller 
- * @param	none
- * 
- */
-void Flash_T::m_reset(void)
+Flash::Flash(QSPI_HandleTypeDef *hqspi)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	
-	cmd.Instruction = 0x66;
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	if(m_QSPI_mode)
-		cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-	else
-		cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;
-	
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		while(1);
-	
-	m_wait();
-	cmd.Instruction = 0x99;
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		while(1);
-
+    qspi_dev            = hqspi;
+    data_mode           = QSPI_DATA_1_LINE;
+    instruction_mode    = QSPI_INSTRUCTION_1_LINE;
+    id                  = 0;
 }
 
-bool Flash_T::m_write_enable(void)
+void Flash::reset(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	QSPI_AutoPollingTypeDef cfg = {0};
-		
-	cmd.Instruction = 0x06;
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	if(m_QSPI_mode == QSPI)
-		cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-	else
-		cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		return false;
-	cfg.Match = 0x02;
-	cfg.Mask = 0x02;
-	cfg.Interval = 0x10;
-	cfg.MatchMode = QSPI_MATCH_MODE_AND;
-	cfg.StatusBytesSize = 1;
-	cfg.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE;
-	cmd.Instruction = 0x05;
-	if(m_QSPI_mode == QSPI)
-		cmd.DataMode = QSPI_DATA_4_LINES;
-	else
-		cmd.DataMode = QSPI_DATA_1_LINE;
-	
-	cmd.NbData = 1;
-	
-	if(HAL_QSPI_AutoPolling(&hqspi, &cmd, &cfg, 100) != HAL_OK){
-		return false;
-	}
-	return true;
-}
+    QSPI_CommandTypeDef cmd = {0};
 
-/**
- * @brief	exit quad mode, set member QSPI_mode to SPI(false)
- * @param	none
- */
-void Flash_T::m_exit_quad_mode(void)
-{
-	QSPI_CommandTypeDef cmd = {0};
-	cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-    cmd.Instruction = 0xFF;
-    HAL_QSPI_Command(&hqspi, &cmd, 100);
-	m_QSPI_mode = SPI;
-}
+    cmd.Instruction     = 0x66;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.InstructionMode = instruction_mode;
 
-/**
- * @brief	enter quad spi mode and set member QSPI_mode to QSPI(true)
- * @param	none
- *  
- */
-void Flash_T::m_set_quad_mode(void)
-{
-	uint8_t tmp = 0;
-	m_read_register(&tmp, 2);
-	if((tmp & 0x2) == 0)
-	{
-		tmp |= 0x02;
-		m_write_register(tmp, 2);
-	}
-	if(((tmp >> 1) & 0x1) != 1)
-		while(1);
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        while (true);
+    }
+    wait();
 
-	//enter quad mode
-	QSPI_CommandTypeDef cmd = {0};
-	cmd.Instruction = 0x38;
-	cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		while(1);
-	m_QSPI_mode = QSPI;
-
-	//set ReadParam
-	cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-    cmd.Instruction = 0xC0;
-    cmd.DataMode = QSPI_DATA_4_LINES;
-    cmd.NbData = 1;
-	tmp = 0x03 << 4;
-	m_write_enable();
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) == HAL_OK)
-    {
-        HAL_QSPI_Transmit(&hqspi, &tmp, 100);
+    cmd.Instruction = 0x99;
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        while (true);
     }
 }
 
-/**
- * @brief	read w25q64 id
- * @param	none
- * @retval	id if read success and HAL_ERROR if sending message error
- * @note	you can add something to detect whether what you have read is the correct id 
- */
-uint16_t Flash_T::m_readJEDECID(void)
+bool Flash::write_enable(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	uint8_t tmp[2] = {0};
-	uint16_t ret = 0;
-	cmd.Instruction = 0x90;
-	if(m_QSPI_mode == QSPI)
-		{cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;cmd.DataMode = QSPI_DATA_4_LINES;}
-	else
-		{cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;cmd.DataMode = QSPI_DATA_1_LINE;}
-	cmd.Address = 0;
-	cmd.AddressMode = QSPI_ADDRESS_4_LINES;
-	cmd.AddressSize = QSPI_ADDRESS_24_BITS;		
-	
-	cmd.NbData = 2;
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		return HAL_ERROR;
-	
-	if(HAL_QSPI_Receive(&hqspi, tmp, 100) != HAL_OK)
-		return HAL_ERROR;
-	ret |= tmp[0] << 8;
-	ret |= tmp[1] << 0;
-	return ret;
+    QSPI_CommandTypeDef cmd = {0};
+    QSPI_AutoPollingTypeDef cfg = {0};
+
+    cmd.Instruction     = 0x06;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.InstructionMode = instruction_mode;
+
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        return false;
+    }
+
+    cfg.Match           = 0x02;
+    cfg.Mask            = 0x02;
+    cfg.Interval        = 0x10;
+    cfg.MatchMode       = QSPI_MATCH_MODE_AND;
+    cfg.StatusBytesSize = 1;
+    cfg.AutomaticStop   = QSPI_AUTOMATIC_STOP_ENABLE;
+
+    cmd.Instruction = 0x05;
+    cmd.DataMode    = data_mode;
+    cmd.NbData      = 1;
+
+    if (HAL_QSPI_AutoPolling(qspi_dev, &cmd, &cfg, 100) != HAL_OK) {
+        return false;
+    }
+
+    return true;
 }
 
-bool Flash_T::m_read_register(uint8_t * rbuffer, uint16_t RegisterN)
+void Flash::disable_quad_mode(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	
-	switch(RegisterN)
-	{
-		case 1:
-			cmd.Instruction = 0x05;break;
-		case 2:
-			cmd.Instruction = 0x35;break;
-		case 3:
-			cmd.Instruction = 0x15;break;
-	}
-	if(m_QSPI_mode == QSPI)
-		{cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;cmd.DataMode = QSPI_DATA_4_LINES;}
-	else
-		{cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;cmd.DataMode = QSPI_DATA_1_LINE;}
-	cmd.NbData = 1;
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		return false;
-	
-	if(HAL_QSPI_Receive(&hqspi, rbuffer, 100) != HAL_OK)
-		return false;
-	
-	return true;
-	
+    QSPI_CommandTypeDef cmd = {0};
+
+    cmd.InstructionMode = instruction_mode;
+    cmd.Instruction     = 0xFF;
+
+    HAL_QSPI_Command(qspi_dev, &cmd, 100);
+
+    data_mode           = QSPI_DATA_1_LINE;
+    instruction_mode    = QSPI_INSTRUCTION_1_LINE;
 }
 
-bool Flash_T::m_write_register(uint8_t data, uint16_t RegisterN)
+void Flash::enable_quad_mode(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	
-	switch(RegisterN)
-	{
-		case 1:
-			cmd.Instruction = 0x01;break;
-		case 2:
-			cmd.Instruction = 0x31;break;
-		case 3:
-			cmd.Instruction = 0x11;break;
-		default:
-			return false;
-	}
-	m_write_enable();
-	if(m_QSPI_mode == QSPI)
-		{cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;cmd.DataMode = QSPI_DATA_4_LINES;}
-	else
-		{cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;cmd.DataMode = QSPI_DATA_1_LINE;}
-	cmd.NbData = 1;
-	
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	if(HAL_QSPI_Command(&hqspi, &cmd, 10) != HAL_OK)
-		return false;
-	uint8_t tmp = data;
-	if(HAL_QSPI_Transmit(&hqspi, &tmp, 1000) != HAL_OK)
-		return false;
-	return true;
+    uint8_t tmp = 0;
 
+    read_reg(&tmp, 2);
+    if ((tmp & 0x02) == 0) {
+        tmp |= 0x02;
+        write_reg(&tmp, 2);
+    }
+
+    if (((tmp >> 1) & 0x01) != 1) {
+        while (true);
+    }
+
+    //enter quad mode
+    QSPI_CommandTypeDef cmd = {0};
+
+    cmd.Instruction     = 0x38;
+    cmd.InstructionMode = instruction_mode;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        while (true);
+    }
+
+    data_mode           = QSPI_DATA_4_LINES;
+    instruction_mode    = QSPI_INSTRUCTION_4_LINES;
+
+    cmd.Instruction     = 0xC0;
+    cmd.InstructionMode = instruction_mode;
+    cmd.DataMode        = data_mode;
+    cmd.NbData          = 1;
+
+    tmp = 0x03 << 4;
+    write_enable();
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) == HAL_OK) {
+        HAL_QSPI_Transmit(qspi_dev, &tmp, 100);
+    }
 }
 
-bool Flash_T::m_wait(void)
+uint16_t Flash::read_jedecid(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	QSPI_AutoPollingTypeDef cfg = {0};
-	if(m_QSPI_mode == QSPI)
-		{cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;cmd.DataMode = QSPI_DATA_4_LINES;}
-	else
-		{cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;cmd.DataMode = QSPI_DATA_1_LINE;}
-	cmd.Instruction = 0x05; //read register 1
-	cmd.NbData = 1; //read one byte
-    cmd.AddressSize = QSPI_ADDRESS_24_BITS;
+    uint8_t tmp[2] = {0};
+    uint16_t ret = 0;
 
-	//mask setting
-	cfg.Mask = 0x01; //detect the busy bit
-	cfg.Match = 0x00; //device is idle if bit busy is set as 0
-	cfg.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE; //stop sending cmd if match
-	cfg.Interval = 0x10; //time between two send
-	cfg.MatchMode = QSPI_MATCH_MODE_AND; //don't care when detect only one bit
-	cfg.StatusBytesSize = 1; //one byte 
-	if(HAL_QSPI_AutoPolling(&hqspi, &cmd, &cfg, 1000) != HAL_OK)
-		return false;
-	return true;
+    QSPI_CommandTypeDef cmd = {0};
+
+    cmd.Instruction     = 0x90;
+    cmd.InstructionMode = instruction_mode;
+    cmd.DataMode        = data_mode;
+    cmd.AddressMode     = QSPI_ADDRESS_4_LINES;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.NbData          = 2;
+
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        return HAL_ERROR;
+    }
+
+    if (HAL_QSPI_Receive(qspi_dev, tmp, 100) != HAL_OK) {
+        return HAL_ERROR;
+    }
+
+    ret |= tmp[0] << 8;
+    ret |= tmp[1] << 0;
+
+    return ret;
 }
 
-Flash_T::Flash_T(void)
+bool Flash::read_reg(uint8_t *data, uint16_t reg)
 {
-	m_QSPI_mode = SPI;
-	m_id = 0;
+    QSPI_CommandTypeDef cmd = {0};
+
+    switch (reg) {
+        case 1:
+            cmd.Instruction = 0x05;
+            break;
+
+        case 2:
+            cmd.Instruction = 0x35;
+            break;
+
+        case 3:
+            cmd.Instruction = 0x15;
+            break;
+    }
+
+    cmd.InstructionMode = instruction_mode;
+    cmd.DataMode        = data_mode;
+    cmd.NbData          = 1;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        return false;
+    }
+
+    if (HAL_QSPI_Receive(qspi_dev, data, 100) != HAL_OK) {
+        return false;
+    }
+
+    return true;
 }
 
-void Flash_T::init(void)
+bool Flash::write_reg(uint8_t *data, uint16_t reg)
 {
-	m_exit_quad_mode();
-	m_reset();
-	m_set_quad_mode();
-	m_id = m_readJEDECID();
+    QSPI_CommandTypeDef cmd = {0};
+
+    switch (reg) {
+        case 1:
+            cmd.Instruction = 0x01;
+            break;
+
+        case 2:
+            cmd.Instruction = 0x31;
+            break;
+
+        case 3:
+            cmd.Instruction = 0x11;
+            break;
+
+        default:
+            return false;
+    }
+
+    write_enable();
+
+    cmd.InstructionMode = instruction_mode;
+    cmd.DataMode        = data_mode;
+    cmd.NbData          = 1;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 10) != HAL_OK) {
+        return false;
+    }
+
+    if (HAL_QSPI_Transmit(qspi_dev, data, 1000) != HAL_OK) {
+        return false;
+    }
+
+    return true;
 }
 
-bool Flash_T::read_N_bytes(uint32_t N, uint32_t address, uint8_t * rbuffer)
+void Flash::init(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	
-	if(address > 0xFFFFFF)
-		return false;
-	
-	cmd.Instruction = 0x0B;
-	if(m_QSPI_mode == QSPI)
-		cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-	else
-		cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;
-	
-	cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	cmd.Address = address;
-	cmd.AddressMode = QSPI_ADDRESS_4_LINES;
-	
-	cmd.DataMode = QSPI_DATA_4_LINES;
-	cmd.NbData = N;
-	
-	cmd.DummyCycles = 8;
-	
-	if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-		return false;
-	if(HAL_QSPI_Receive(&hqspi, rbuffer, 100) != HAL_OK)
-		return false;
-	
-	return true;
+    disable_quad_mode();
+    reset();
+    enable_quad_mode();
+    id = read_jedecid();
 }
 
-bool Flash_T::write_N_bytes(uint32_t N, uint32_t address, uint8_t * sbuffer)
+bool Flash::wait(void)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	uint32_t end_addr, current_addr = 0x00, current_size;
-	uint8_t * current_buffer = sbuffer;
-	
-	if(address > 0xFFFFFF) //detect if address bigger than max address value
-		return false;
-	while(current_addr <= address){ //current address increats until bigger than the passed address
-		current_addr += 0x100; //increment is a page 256 bytes
-	}
-	current_size = current_addr - address; //now current_size is the blank space of last page to be programed
-	if(current_size >= N)
-	{
-		current_size = N;
-	}
-	
-	current_addr = address;
-	end_addr = address + N;
-	
-	/* cmd config */
-	if(m_QSPI_mode == QSPI)
-		{cmd.Instruction = 0x02;cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;}
-	else
-		{cmd.Instruction = 0x32;cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;}
-	
-	cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	cmd.Address = current_addr;
-	if(m_QSPI_mode == QSPI)
-		cmd.AddressMode = QSPI_ADDRESS_4_LINES;
-	else
-		cmd.AddressMode = QSPI_ADDRESS_1_LINE;
-	
-	cmd.DataMode = QSPI_DATA_4_LINES;
-	cmd.NbData = current_size;
-	
-	
-	do
-	{
-		m_write_enable();
-  		m_set_quad_mode();
-		
-		if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-			return false;
-		if(HAL_QSPI_Transmit(&hqspi, current_buffer, 10000) != HAL_OK)
-			return false;
-		current_addr += current_size;
-		current_buffer += current_size;
-		
-		current_size = end_addr - current_addr;
-		if(current_size > 0x100)
-		{
-			/*check if the rest bytes bigger than 0x100*/
-			current_size = 0x100;
-		}
-		
-		cmd.NbData = current_size;
-		cmd.Address = current_addr;
-	}while(end_addr > cmd.Address);
-	return true;
+    QSPI_CommandTypeDef cmd = {0};
+    QSPI_AutoPollingTypeDef cfg = {0};
+
+    cmd.InstructionMode = instruction_mode;
+    cmd.DataMode        = data_mode;
+    cmd.Instruction     = 0x05;                 //read register 1
+    cmd.NbData          = 1;                    //read one byte
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+
+    //mask setting
+    cfg.Mask            = 0x01;                         //detect the busy bit
+    cfg.Match           = 0x00;                         //device is idle if bit busy is set as 0
+    cfg.AutomaticStop   = QSPI_AUTOMATIC_STOP_ENABLE;   //stop sending cmd if match
+    cfg.Interval        = 0x10;                         //time between two send
+    cfg.MatchMode       = QSPI_MATCH_MODE_AND;          //don't care when detect only one bit
+    cfg.StatusBytesSize = 1;
+    if (HAL_QSPI_AutoPolling(qspi_dev, &cmd, &cfg, 1000) != HAL_OK) {
+        return false;
+    }
+
+    return true;
 }
 
-bool Flash_T::sector_erase(uint32_t start, uint32_t end)
+bool Flash::read_data(uint8_t *data, uint32_t addr, uint32_t len)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	uint16_t sector_start = 0, sector_end = 0;
-	sector_start = start / 4096; //start is the num of the first sector
-	sector_end = end / 4096; //end is the num of the last sector
-	cmd.Instruction = 0x20;	
-	cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	if(m_QSPI_mode == QSPI)
-		{cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;cmd.AddressMode = QSPI_ADDRESS_4_LINES;}
-	else
-		{cmd.InstructionMode = QSPI_INSTRUCTION_1_LINE;cmd.AddressMode = QSPI_ADDRESS_1_LINE;}
-	do
-	{
-		m_write_enable();
-		cmd.Address = sector_start * 4096; //sector increse
-		if(HAL_QSPI_Command(&hqspi, &cmd, 100) != HAL_OK)
-			return false;
-		sector_start++;
-		m_wait();
-	}while(sector_start <= sector_end);
-	
-	return true;
+    if (addr > 0xFFFFFF) {
+        return false;
+    }
+
+    QSPI_CommandTypeDef cmd = {0};
+
+    cmd.Instruction     = 0x0B;
+    cmd.InstructionMode = instruction_mode;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.Address         = addr;
+    cmd.AddressMode     = QSPI_ADDRESS_4_LINES;
+    cmd.DataMode        = data_mode;
+    cmd.NbData          = len;
+    cmd.DummyCycles     = 8;
+
+    if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+        return false;
+    }
+
+    if (HAL_QSPI_Receive(qspi_dev, data, 100) != HAL_OK) {
+        return false;
+    }
+
+    return true;
 }
 
-/**
- * @brief	enter memory map mode
- * @param	none
- * @note	you can choose 0xEB or 0x0B for read command 
- */
-void Flash_T::memory_map(void)
+bool Flash::write_data(uint8_t *data, uint32_t addr, uint32_t len)
 {
-	QSPI_CommandTypeDef cmd = {0};
-	QSPI_MemoryMappedTypeDef cfg = {0};
+    if (addr > 0xFFFFFF) { //detect if address bigger than max address value
+        return false;
+    }
 
-	cmd.InstructionMode = QSPI_INSTRUCTION_4_LINES;
-	cmd.Instruction = 0xEB; //quad fast read
-	cmd.AddressMode = QSPI_ADDRESS_4_LINES;
-	cmd.AddressSize = QSPI_ADDRESS_24_BITS;
-	cmd.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
-	cmd.DataMode = QSPI_DATA_4_LINES;
-	cmd.DummyCycles = 8;
+    QSPI_CommandTypeDef cmd = {0};
+    uint32_t end_addr, current_addr = 0, current_size;
+    uint8_t *current_buffer = data;
 
-	cfg.TimeOutActivation = QSPI_TIMEOUT_COUNTER_DISABLE;
-  	cfg.TimeOutPeriod = 0;
- 
-	if (HAL_QSPI_MemoryMapped(&hqspi, &cmd, &cfg) != HAL_OK) {
-        while (1);
-	}
+    while (current_addr <= addr) { //current address increats until bigger than the passed address
+        current_addr += 0x0100; //increment is a page 256 bytes
+    }
+
+    current_size = current_addr - addr; //now current_size is the blank space of last page to be programed
+    if (current_size >= len) {
+        current_size = len;
+    }
+
+    current_addr    = addr;
+    end_addr        = addr + len;
+
+    /* cmd config */
+    if (instruction_mode == QSPI_INSTRUCTION_4_LINES) {
+        cmd.Instruction = 0x02;
+        cmd.AddressMode = QSPI_ADDRESS_4_LINES;
+    } else {
+        cmd.Instruction = 0x32;
+        cmd.AddressMode = QSPI_ADDRESS_1_LINE;
+    }
+
+    cmd.InstructionMode = instruction_mode;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.Address         = current_addr;
+    cmd.DataMode        = data_mode;
+    cmd.NbData          = current_size;
+
+    do {
+        write_enable();
+        enable_quad_mode();
+
+        if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+            return false;
+        }
+
+        if (HAL_QSPI_Transmit(qspi_dev, current_buffer, 10000) != HAL_OK) {
+            return false;
+        }
+
+        current_addr    += current_size;
+        current_buffer  += current_size;
+        current_size    = end_addr - current_addr;
+
+        if (current_size > 0x100) {
+            /*check if the rest bytes bigger than 0x100*/
+            current_size = 0x100;
+        }
+
+        cmd.NbData  = current_size;
+        cmd.Address = current_addr;
+    } while (end_addr > cmd.Address);
+
+    return true;
+}
+
+bool Flash::sector_erase(uint32_t start, uint32_t end)
+{
+    QSPI_CommandTypeDef cmd = {0};
+
+    uint16_t sector_start = start / 4096; //start is the num of the first sector
+    uint16_t sector_end = end / 4096; //end is the num of the last sector
+
+    cmd.Instruction     = 0x20;
+    cmd.AddressSize     = QSPI_ADDRESS_24_BITS;
+    cmd.InstructionMode = instruction_mode;
+
+    if (instruction_mode == QSPI_INSTRUCTION_4_LINES) {
+        cmd.AddressMode = QSPI_ADDRESS_4_LINES;
+    } else {
+        cmd.AddressMode = QSPI_ADDRESS_1_LINE;
+    }
+
+    do {
+        write_enable();
+        cmd.Address = sector_start * 4096; //sector increse
+        if (HAL_QSPI_Command(qspi_dev, &cmd, 100) != HAL_OK) {
+            return false;
+        }
+        sector_start++;
+        wait();
+    } while(sector_start <= sector_end);
+
+    return true;
+}
+
+void Flash::enable_memory_map(void)
+{
+    QSPI_CommandTypeDef cmd = {0};
+    QSPI_MemoryMappedTypeDef cfg = {0};
+
+    cmd.InstructionMode     = instruction_mode;
+    cmd.Instruction         = 0xEB; //quad fast read
+    cmd.AddressMode         = QSPI_ADDRESS_4_LINES;
+    cmd.AddressSize         = QSPI_ADDRESS_24_BITS;
+    cmd.AlternateByteMode   = QSPI_ALTERNATE_BYTES_NONE;
+    cmd.DataMode            = data_mode;
+    cmd.DummyCycles         = 8;
+
+    cfg.TimeOutActivation   = QSPI_TIMEOUT_COUNTER_DISABLE;
+    cfg.TimeOutPeriod       = 0;
+
+    if (HAL_QSPI_MemoryMapped(qspi_dev, &cmd, &cfg) != HAL_OK) {
+        while (true);
+    }
 }
--- src/drivers/w25q/w25q.h
+++ src/drivers/w25q/w25q.h
@@ -1,32 +1,33 @@
+#pragma once
+
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_hal_qspi.h"
 
-#define W25Q64_H
-#ifdef W25Q64_H
-
-#define QSPI true
-#define SPI false
-
-class Flash_T
+namespace W25Q
 {
-private:
-    bool m_QSPI_mode;
-    uint16_t m_id;
-    void m_reset(void);
-    bool m_write_enable(void);
-    void m_exit_quad_mode(void);
-    void m_set_quad_mode(void);
-    uint16_t m_readJEDECID(void);
-    bool m_read_register(uint8_t * rbuffer, uint16_t RegisterN);
-    bool m_write_register(uint8_t data, uint16_t RegisterN);
-    bool m_wait(void);
-public:
-    Flash_T(void);
-    void init(void);
-    bool read_N_bytes(uint32_t N, uint32_t address, uint8_t * rbuffer);
-    bool write_N_bytes(uint32_t N, uint32_t address, uint8_t * sbuffer);
-    bool sector_erase(uint32_t start, uint32_t end);
-    void memory_map(void);
-};
+    class Flash
+    {
+        private:
+        QSPI_HandleTypeDef *qspi_dev;
+        uint32_t data_mode;
+        uint32_t instruction_mode;
+        uint16_t id;
+        void reset(void);
+        bool write_enable(void);
+        void disable_quad_mode(void);
+        void enable_quad_mode(void);
+        uint16_t read_jedecid(void);
+        bool read_reg(uint8_t *data, uint16_t reg);
+        bool write_reg(uint8_t *data, uint16_t reg);
+        bool wait(void);
 
-#endif
+        public:
+        Flash(QSPI_HandleTypeDef *hqspi);
+        void init(void);
+        bool read_data(uint8_t *data, uint32_t addr, uint32_t len);
+        bool write_data(uint8_t *data, uint32_t addr, uint32_t len);
+        bool sector_erase(uint32_t start, uint32_t end);
+        void enable_memory_map(void);
+    };
+}
+
