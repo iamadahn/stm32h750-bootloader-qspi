1263ec58ed7f99e0d60dfdbec44082fc664233cb
iamadahn <vendonism16@yandex.ru>
2025-04-12 22:36:07 +0300

    flash: updated due to weird error with one sector overwriting another

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-openocd -f interface/stlink.cfg -f target/stm32h7x.cfg -c "program build/bootloader.elf verify reset exit"
+openocd -f interface/stlink.cfg -f target/stm32h7x.cfg -c "program build/bootloader.bin 0x8000000 verify reset exit"
